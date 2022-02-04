b2ccebd06a928f95d1c44738c760bd440d9e62f7
Akshara Kuduvalli <akshara.kuduvalli@gmail.com>
2022-02-03 17:22:06 -0800

    added junit libraries

--- SkillDemo1.java
+++ SkillDemo1.java
@@ -1,1 +1,8 @@
 //file
+
+
+public class SkillDemo1 {
+	public static String concatWithSpace(String first, String second) {
+		return first + second; //fails because forgot the space as per the method description
+	}
+}
--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -1,1 +1,10 @@
 //test
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+	@Test
+    public void testConcatSpace() {
+        assertEquals("birthday cake", SkillDemo1.concatWithSpace("birthday","cake"));
+    }
+}
