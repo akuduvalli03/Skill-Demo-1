892e091be944a6cbcee8c4ed7a69bfc1fb27517c
Akshara Kuduvalli <akshara.kuduvalli@gmail.com>
2022-02-03 17:26:30 -0800

    fixed error

--- SkillDemo1.java
+++ SkillDemo1.java
@@ -3,6 +3,6 @@
 
 public class SkillDemo1 {
 	public static String concatWithSpace(String first, String second) {
-		return first + second; //fails because forgot the space as per the method description
+		return first + " " + second; //fails because forgot the space as per the method description
 	}
 }
